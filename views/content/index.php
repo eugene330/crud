e5ac2635e4371903d5160055151a986c0fbff9ed
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2020-05-31 20:15:13 +0300

    add table form

--- views/content/index.php
+++ views/content/index.php
@@ -1,1 +1,8 @@
-<h2>Articles</h2>
+<div class="row">
+    <div class="card offset-md-3 col-md-6">
+        <div class="card-header">Articles</div>
+        <div class="card-body">
+            <a href="?action=create"><i class="fa fa-plus">Create new article</i></a>
+        </div>
+    </div>
+</div>
